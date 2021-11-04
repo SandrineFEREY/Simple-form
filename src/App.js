cc43e9a4bf78158e302c3a587fb35e07a449e2a7
Sandrine FEREY <sandrineferey@air-de-sandrine.lan>
2021-11-04 16:41:27 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+library.add(faUser);
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [register, setRegister] = useState(false);
+
+  const handleName = (event) => {
+    const value = event.target.value;
+    setName(value);
+  };
+
+  const handleEmail = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+
+  const handlePassword = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+  };
+
+  const handleConfirmPassword = (event) => {
+    const value = event.target.value;
+    setConfirmPassword(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Create account</h1>
       </header>
+      <div>
+        {register === false ? (
+          <form
+            onSubmit={() => {
+              password !== confirmPassword
+                ? alert("Vos deux mots de passe ne sont pas identiques")
+                : setRegister(true);
+            }}
+          >
+            <div>
+              <h2>Name</h2>
+              <input
+                onChange={handleName}
+                type="text"
+                placeholder="name"
+                value={name}
+              />
+            </div>
+            <div>
+              <h2>Email</h2>
+              <input
+                onChange={handleEmail}
+                type="text"
+                placeholder="email"
+                value={email}
+              />
+            </div>
+            <div>
+              <h2>Password</h2>
+              <input
+                onChange={handlePassword}
+                type="password"
+                placeholder="password"
+                value={password}
+              />
+            </div>
+            <div>
+              <h2>Confirm your passeword</h2>
+              <input
+                onChange={handleConfirmPassword}
+                type="password"
+                placeholder="confirmPassword"
+                value={confirmPassword}
+              />
+            </div>
+            <div>
+              <input className="submit" type="submit" value="Register" />
+            </div>
+          </form>
+        ) : (
+          <div>
+            <h2>Results</h2>
+            <p>Name : {name}</p>
+            <p>Email : {email}</p>
+            <p>Password : {password}</p>
+            <button
+              onClick={() => {
+                setRegister(false);
+              }}
+            >
+              Edit your information
+            </button>
+          </div>
+        )}
+      </div>
+
+      <footer>
+        <p>
+          Made with <span>Reacteur</span> at <span>Le Reacteur</span> by{" "}
+          <span>Brice</span>{" "}
+        </p>
+      </footer>
     </div>
   );
 }
